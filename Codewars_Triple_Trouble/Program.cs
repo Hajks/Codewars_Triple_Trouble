35a22d1e2ceeaf634c9b020b8c379b2de36f3aa3
Hajk <tunczorx@gmail.com>
2018-10-09 23:01:16 +0200

    Add project files.

---
+++ Codewars_Triple_Trouble/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars_Triple_Trouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
